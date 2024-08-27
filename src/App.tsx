27b2fb328435ba4b710b4682a2a1116b80939cee
griminir <viktor.elliot.king@gmail.com>
2024-08-27 11:12:41 +0200

    how to access input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import ExpandableText from './components/ExpandableText';
+
 import Form from './components/Form';
 function App() {
   return (
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,39 @@
-import { FormEvent } from 'react';
+import { FormEvent, useRef } from 'react';
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = {name: '', age: 0 }
+
   const handleSubmit = (event: FormEvent) => {
-      event.preventDefault();
-      console.log('submitted')
-  }
+    event.preventDefault();
+    if (nameRef.current !== null)
+      person.name = nameRef.current.value
+    if (ageRef.current !== null)
+      person.age =  parseInt(ageRef.current.value);
+    console.log(person);
+    
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-lable">Name</label>
-        <input id='name' type="text" className="form-control" />
+      <div className='mb-3'>
+        <label htmlFor='name' className='form-lable'>
+          Name
+        </label>
+        <input ref={nameRef} id='name' type='text' className='form-control' />
       </div>
-      <div className="mb-3">
-        <label htmlFor="age" className="form-label">Age</label>
-        <input id='age' type="number" className="form-control" />
-      </div> 
-      <button type='submit' className="btn btn-primary">Submit</button>
+      <div className='mb-3'>
+        <label htmlFor='age' className='form-label'>
+          Age
+        </label>
+        <input ref={ageRef} id='age' type='number' className='form-control' />
+      </div>
+      <button type='submit' className='btn btn-primary'>
+        Submit
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
